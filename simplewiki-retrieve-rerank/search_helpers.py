b170564ce5ea9e83b57d76eec919ca8c893a3309
Yahya Alaa <yahyaalaamassoud@gmail.com>
2024-03-25 06:40:32 -0700

    Update search_helpers.py
    - retrieve top k passages using binary quantization
    - return runtime along with retrieval and reranking results

--- simplewiki-retrieve-rerank/search_helpers.py
+++ simplewiki-retrieve-rerank/search_helpers.py
@@ -1,36 +1,50 @@
-from collections import OrderedDict
 from qdrant_client import models
 
 def retrieve_top_k(query, top_k, vec_db, encoder, collection_name):
+  st = time.time()
   results = vec_db.search(
     collection_name=collection_name,
     query_vector=encoder.encode(query),
     limit=top_k,
-    with_payload=True,
-    search_params=models.SearchParams(exact=False),
+    search_params=models.SearchParams(
+      quantization=models.QuantizationSearchParams(
+        ignore=False,
+        rescore=True,
+        oversampling=2.0,
+      )
+    )
   ) # search for top K passages from simplewiki
+  query_time = time.time() - st
 
   top_hits = []
   for i, hit in enumerate(results):
     top_hits.append({'passage': hit.payload['passage'], 'article_id': hit.payload['article_id'], 'score': hit.score, 'order': (len(results) - i) / len(results)})
 
-  return top_hits
+  return top_hits, query_time
 
-def rerank_hits(query, hits, cross_encoder):
+def rerank_hits(query, hits, cross_encoder, articles):
+  hits_order = {}
+  for i, hit in enumerate(hits):
+    hits_order[hit['article_id']] = i + 1
+
   cross_inp = [[query, hit['passage']] for hit in hits]
+  st = time.time()
   cross_scores = cross_encoder.predict(cross_inp)
   hits_with_scores = list(zip(hits, cross_scores))
   sorted_hits = sorted(hits_with_scores, key=lambda x: x[1], reverse=True)
+  reranking_time = time.time() - st
   reranked_hits = []
   for i, (hit, cross_score) in enumerate(sorted_hits):
     reranked_hits.append({
+        'title': articles[hit['article_id']]['title'],
         'article_id': hit['article_id'],
         'passage': hit['passage'],
         'score': cross_score,
         'original_score': hit['score'],
-        'order': (len(sorted_hits) - i) / len(sorted_hits),
+        'retrieval_order': hits_order[hit['article_id']],
+        'reranked_order': i + 1
     })
-  return reranked_hits
+  return reranked_hits, reranking_time
 
 def extract_sentence_and_partition(text, sentence):
     # Ensure the sentence exists in the text
@@ -71,12 +85,3 @@ def fetch_top_article_with_passage_highlighted(hits, articles):
   top_article = articles[top_hit['article_id']]['content']
   top_passage = top_hit['passage']
   return extract_sentence_and_partition(top_article, top_passage)
-
-def fetch_article_title_with_order(hits, articles):
-  if not hits:
-    return None
-  titles = OrderedDict()
-  for hit in hits:
-    if articles[hit['article_id']]['title'] not in titles:
-      titles[articles[hit['article_id']]['title']] = hit['order']
-  return titles
